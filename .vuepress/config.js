78d97d6bcee579cb3a6f63a2bc12fa5fe6888e83
TomS-Developer <tom@tomdeveloper.xyz>
2021-01-21 15:12:23 +0000

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
     title: 'Tomas.GG Documentation',
     description: 'API & Other documentation for Tomas.GG services.',
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "https://api.slushie.gg/avatar/683781987726917663?size=256"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "https://api.slushie.gg/avatar/683781987726917663?size=256"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "https://api.slushie.gg/avatar/683781987726917663?size=256"}],
+      ],
     themeConfig: {
         nav: [
             { text: 'Website', link: 'https://tomas.gg'},
